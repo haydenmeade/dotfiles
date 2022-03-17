0df787d0884c0f6217d4539e181c987e75efcc16
haydenmeade <haydenmeade@gmail.com>
2022-03-18 09:53:49 +1100

    cpp lsp

--- nvim/lua/config/lsp/init.lua
+++ nvim/lua/config/lsp/init.lua
@@ -1,35 +1,36 @@
 local M = {}
 
 local lsp_providers = {
-  pyright = true,
-  gopls = true,
-  sumneko_lua = true,
+	pyright = true,
+	gopls = true,
+	sumneko_lua = true,
+	clangd = true,
 }
 
 local function setup_servers()
-  local lsp_installer = require "nvim-lsp-installer"
+	local lsp_installer = require("nvim-lsp-installer")
 
-  require("config.lsp.null-ls").setup()
+	require("config.lsp.null-ls").setup()
 
-  lsp_installer.on_server_ready(function(server)
-    if lsp_providers[server.name] then
-      local server_opts = require("config.lsp." .. server.name).setup(server)
-      if server_opts then
-        server:setup(server_opts)
-      end
-    else
-      local lsputils = require "config.lsp.utils"
-      local opts = {
-        on_attach = lsputils.lsp_attach,
-        capabilities = lsputils.get_capabilities(),
-      }
-      server:setup(opts)
-    end
-  end)
+	lsp_installer.on_server_ready(function(server)
+		if lsp_providers[server.name] then
+			local server_opts = require("config.lsp." .. server.name).setup(server)
+			if server_opts then
+				server:setup(server_opts)
+			end
+		else
+			local lsputils = require("config.lsp.utils")
+			local opts = {
+				on_attach = lsputils.lsp_attach,
+				capabilities = lsputils.get_capabilities(),
+			}
+			server:setup(opts)
+		end
+	end)
 end
 
 function M.setup()
-  setup_servers()
+	setup_servers()
 end
 
 return M
