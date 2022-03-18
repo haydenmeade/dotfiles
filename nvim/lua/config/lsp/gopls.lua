3632951160f29c0f8690613e72bad0e2af7c9d64
haydenmeade <haydenmeade@gmail.com>
2022-03-18 15:15:06 +1100

    add neogit and gitmessenger

--- nvim/lua/config/lsp/gopls.lua
+++ nvim/lua/config/lsp/gopls.lua
@@ -1,33 +1,32 @@
 local M = {}
 
-local lsputils = require "config.lsp.utils"
+local lsputils = require("config.lsp.utils")
 
 function M.config(installed_server)
-  return {
-    lspconfig = {
-      capabilities = lsputils.get_capabilities(),
-      on_attach = lsputils.lsp_attach,
-      on_init = lsputils.lsp_init,
-      on_exit = lsputils.lsp_exit,
-      cmd_env = installed_server._default_options.cmd_env,
-      flags = { debounce_text_changes = 150 },
-      experimentalPostfixCompletions = true,
-      analyses = { unusedparams = true, unreachable = false },
-      codelenses = { generate = true, gc_details = true, test = true, tidy = true },
-      usePlaceholders = true,
-      completeUnimported = true,
-      staticcheck = true,
-      matcher = "fuzzy",
-      experimentalDiagnosticsDelay = "500ms",
-      symbolMatcher = "fuzzy",
-      gofumpt = true,
-      buildFlags = { "-tags", "integration" },
-    },
-  }
+	return {
+		lspconfig = {
+			capabilities = lsputils.get_capabilities(),
+			on_attach = lsputils.lsp_attach,
+			on_init = lsputils.lsp_init,
+			on_exit = lsputils.lsp_exit,
+			cmd_env = installed_server._default_options.cmd_env,
+			flags = { debounce_text_changes = 150 },
+			experimentalPostfixCompletions = true,
+			analyses = { unusedparams = true, unreachable = false },
+			codelenses = { generate = true, gc_details = true, test = true, tidy = true },
+			usePlaceholders = true,
+			completeUnimported = true,
+			staticcheck = true,
+			matcher = "fuzzy",
+			experimentalDiagnosticsDelay = "500ms",
+			symbolMatcher = "fuzzy",
+			gofumpt = true,
+		},
+	}
 end
 
 function M.setup(installed_server)
-  return M.config(installed_server)
+	return M.config(installed_server)
 end
 
 return M
