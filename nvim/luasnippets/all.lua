10f11dd0eeec73786e6e5e943b57c82d4d065b0c
haydenmeade <haydenmeade@gmail.com>
2022-03-25 10:13:00 +1100

    file snippet

--- nvim/luasnippets/all.lua
+++ nvim/luasnippets/all.lua
@@ -6,6 +6,7 @@ return {
 	ls.s("time", partial(vim.fn.strftime, "%H:%M:%S")),
 	ls.s("date", partial(vim.fn.strftime, "%Y-%m-%d")),
 	ls.s("pwd", { partial(util.shell, "pwd") }),
+	ls.s("file", { partial(vim.fn.expand, "%") }),
 	ls.s({ trig = "uuid", wordTrig = true }, { ls.f(util.uuid), ls.i(0) }),
 	ls.s({ trig = "rstr(%d+)", regTrig = true }, {
 		ls.f(function(_, snip)
