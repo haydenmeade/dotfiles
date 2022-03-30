9853b9d00cdc6bc24a1e5105d993e02c373cd5c3
haydenmeade <haydenmeade@gmail.com>
2022-03-30 19:39:55 +1100

    Comment

--- nvim/lua/config/comment.lua
+++ nvim/lua/config/comment.lua
@@ -2,7 +2,17 @@ local M = {}
 
 function M.setup()
   require("Comment").setup {
-    mappings = { extra = false },
+    mappings = {
+      -- gco
+      -- gcO
+      -- gcA
+      extra = true,
+      -- gcc
+      -- gcb
+      -- gc[count][motion]
+      -- gb[count][motion]
+      basic = true,
+    },
   }
 end
 
