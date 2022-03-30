4b2ccf6bfe79a3ca5080c6cf42d232261cf4f0fd
haydenmeade <haydenmeade@gmail.com>
2022-03-30 16:57:52 +1100

    Gopls fixing

--- nvim/lua/config/lsp/gopls.lua
+++ nvim/lua/config/lsp/gopls.lua
@@ -4,16 +4,10 @@ function M.config()
   -- https://github.com/golang/tools/blob/master/gopls/doc/settings.md
   return {
     lspconfig = {
-      experimentalPostfixCompletions = true,
       analyses = { unusedparams = true, unreachable = false },
       codelenses = { generate = true, gc_details = true, test = true, tidy = true },
-      usePlaceholders = true,
-      completeUnimported = true,
-      staticcheck = true,
-      matcher = "fuzzy",
-      experimentalDiagnosticsDelay = "500ms",
-      symbolMatcher = "fuzzy",
-      gofumpt = true,
+      experimentalPostfixCompletions = true,
+      gofumpt = false,
     },
   }
 end
