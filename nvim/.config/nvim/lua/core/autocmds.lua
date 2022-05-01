285a0f93ca4abea5c80f1d8093e40a4535cae7a7
Hayden Meade <haydenmeade@gmail.com>
2022-05-02 09:45:27 +1000

    fix format autocmd

--- nvim/.config/nvim/lua/core/autocmds.lua
+++ nvim/.config/nvim/lua/core/autocmds.lua
@@ -52,99 +52,24 @@ vim.api.nvim_exec(
 
 local format_on_save = true
 
-local get_format_on_save_opts = function()
-  return {
-    pattern = "*",
-    timeout = 1000,
-  }
-end
-
---- Disable autocommand groups if it exists
---- This is more reliable than trying to delete the augroup itself
----@param name string the augroup name
-function M.disable_augroup(name)
-  -- defer the function in case the autocommand is still in-use
-  vim.schedule(function()
-    if vim.fn.exists("#" .. name) == 1 then
-      vim.cmd("augroup " .. name)
-      vim.cmd "autocmd!"
-      vim.cmd "augroup END"
-    end
-  end)
-end
-
---- Create autocommand groups based on the passed definitions
----@param definitions table contains trigger, pattern and text. The key will be used as a group name
----@param buffer boolean indicate if the augroup should be local to the buffer
-function M.define_augroups(definitions, buffer)
-  for group_name, definition in pairs(definitions) do
-    vim.cmd("augroup " .. group_name)
-    if buffer then
-      vim.cmd [[autocmd! * <buffer>]]
-    else
-      vim.cmd [[autocmd!]]
-    end
-
-    for _, def in pairs(definition) do
-      local command = table.concat(vim.tbl_flatten { "autocmd", def }, " ")
-      vim.cmd(command)
-    end
-
-    vim.cmd "augroup END"
-  end
-end
-
-function M.enable_format_on_save(opts)
-  local fmd_cmd = string.format(":silent lua vim.lsp.buf.formatting_seq_sync({}, %s)", opts.timeout)
-  M.define_augroups {
-    format_on_save = { { "BufWritePre", opts.pattern, fmd_cmd } },
-  }
-  Log:debug "enabled format-on-save"
-end
-
-function M.disable_format_on_save()
-  M.disable_augroup "format_on_save"
-  Log:debug "disabled format-on-save"
-end
-
 function M.configure_format_on_save()
+  local augid = vim.api.nvim_create_augroup("user_lsp_fmt_on_save", { clear = true })
   if format_on_save then
-    local opts = get_format_on_save_opts()
-    M.enable_format_on_save(opts)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      callback = function()
+        vim.lsp.buf.format { timeout_ms = 2000 }
+      end,
+      group = augid,
+    })
+    Log:debug "enabled format-on-save"
   else
-    M.disable_format_on_save()
+    Log:debug "disabled format-on-save"
   end
 end
 
 function M.toggle_format_on_save()
-  if vim.fn.exists "#format_on_save#BufWritePre" == 0 then
-    local opts = get_format_on_save_opts()
-    M.enable_format_on_save(opts)
-  else
-    M.disable_format_on_save()
-  end
   format_on_save = not format_on_save
+  M.configure_format_on_save()
 end
-
--- function M.enable_lsp_document_highlight(client_id)
---   M.define_augroups({
---     lsp_document_highlight = {
---       {
---         "CursorHold",
---         "<buffer>",
---         string.format("lua require('lsp.utils').conditional_document_highlight(%d)", client_id),
---       },
---       {
---         "CursorMoved",
---         "<buffer>",
---         "lua vim.lsp.buf.clear_references()",
---       },
---     },
---   }, true)
--- end
---
--- function M.disable_lsp_document_highlight()
---   M.disable_augroup "lsp_document_highlight"
--- end
 
 return M
