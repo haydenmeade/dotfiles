203afaf711db6cd4d5b0988044040a8730123769
Hayden Meade <haydenmeade@gmail.com>
2022-05-31 12:05:49 +1000

    no ultest

--- nvim/.config/nvim/lua/config/test.lua
+++ nvim/.config/nvim/lua/config/test.lua
@@ -32,12 +32,12 @@ function M.coverage()
 end
 
 function M.setup()
-  local ok, ultest = h.safe_require "ultest"
-  if not ok then
-    return
-  end
+  -- local ok, ultest = h.safe_require "ultest"
+  -- if not ok then
+  --   return
+  -- end
 
-  vim.g.ultest_use_pty = 1
+  -- vim.g.ultest_use_pty = 1
   vim.g.preserve_screen = 1
   -- vim.g.ultest_virtual_text = 1
   vim.api.nvim_exec(
@@ -46,36 +46,6 @@ function M.setup()
     ]],
     false
   )
-
-  local builders = {
-    -- Go Test Debugging
-    ["go#richgo"] = function(cmd)
-      local args = {}
-
-      for i = 3, #cmd, 1 do
-        local arg = cmd[i]
-        if vim.startswith(arg, "-") then
-          arg = "-test." .. string.sub(arg, 2)
-        end
-        args[#args + 1] = arg
-      end
-      print(vim.inspect(args))
-      return {
-        dap = {
-          type = "go",
-          request = "launch",
-          mode = "test",
-          program = "${workspaceFolder}",
-          dlvToolPath = vim.fn.exepath "dlv",
-          args = args,
-        },
-        parse_result = function(lines)
-          return lines[#lines] == "FAIL" and 1 or 0
-        end,
-      }
-    end,
-  }
-  ultest.setup { builders = builders }
 end
 
 return M
