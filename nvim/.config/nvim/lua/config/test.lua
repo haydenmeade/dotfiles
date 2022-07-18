92b0631a69ecd17035151f4f0dfd201bce01e23a
Hayden Meade <haydenmeade@gmail.com>
2022-07-18 13:51:26 +1000

    coverage in base plugin

--- nvim/.config/nvim/lua/config/test.lua
+++ nvim/.config/nvim/lua/config/test.lua
@@ -13,17 +13,6 @@ function M.coverage()
 
   coverage.setup({
     commands = true,
-    lang = {
-      typescript = {
-        coverage_file = "coverage/lcov.info",
-      },
-      javascript = {
-        coverage_file = "coverage/lcov.info",
-      },
-      go = {
-        coverage_file = "coverage.lcov",
-      },
-    },
     highlights = {
       covered = { fg = colors.green },
       uncovered = { fg = colors.red },
@@ -32,10 +21,10 @@ function M.coverage()
 end
 
 function M.setup()
-  -- local ok, ultest = h.safe_require "ultest"
-  -- if not ok then
-  --   return
-  -- end
+  local ok, neotest = h.safe_require("neotest")
+  if not ok then
+    return
+  end
 
   -- vim.g.ultest_use_pty = 1
   vim.g.preserve_screen = 1
@@ -47,13 +36,9 @@ function M.setup()
     false
   )
 
-  require("neotest").setup({
+  neotest.setup({
     adapters = {
-      require("neotest-jest")({
-        -- jestConfigFile = function(p)
-        --   return ""
-        -- end,
-      }),
+      require("neotest-jest"),
       require("neotest-go"),
       require("neotest-rspec"),
       require("neotest-plenary"),
