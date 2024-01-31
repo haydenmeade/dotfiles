a8f066c3185bc3642be3aa488e81bfd798f597a5
Hayden Meade <haydenmeade@gmail.com>
2024-01-31 12:50:17 +1100

    fix snippets

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -138,6 +138,7 @@ require("lazy").setup({
     "L3MON4D3/LuaSnip",
     version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
     build = "make install_jsregexp",
+    dependencies = { "rafamadriz/friendly-snippets" },
     config = function()
       require("config.luasnip").setup()
     end,
